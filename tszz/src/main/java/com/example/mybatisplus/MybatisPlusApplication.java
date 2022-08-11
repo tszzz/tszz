6b39d258509ac43726b19d0c6ba54c545adf8a9f
wangshi_sh <shi.wang@sihua.tech>
2022-08-11 17:45:13 +0800

    init:初始化

---
+++ tszz/src/main/java/com/example/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package com.example.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tszz
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+}
